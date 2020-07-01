53e5fcdb172bd60d66733c284fe3850c35ac04b4
Patrick Poppe <patrick.poppe94@gmail.com>
2020-07-01 16:18:21 +0200

    user levels

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -2,23 +2,23 @@
   <div class="navigation">
     <div type="button" class="missionReportNav">
       <div id="buttonnav">
-          <img src="../assets/missionReportLogo.png" @click="$router.push('missionReport')"/>
+          <img src="../assets/missionReportLogo.png" @click="$router.push('missionReport')" v-if="aktUser.role == '2.0'"/>
 
       </div>
       <div id="buttonnav">
-          <img src="../assets/hospitalLogo.jpg" @click="$router.push('missionReport')" />
+          <img src="../assets/hospitalLogo.jpg" @click="$router.push('missionReport')" v-if="aktUser.role == '2.0'"/>
       </div>
+      <!-- <div id="buttonnav">
+          <img src="../assets/wayLogo.png" @click="$router.push('showWay')" v-if="aktUser.role == '2.0'"/>
+      </div> -->
       <div id="buttonnav">
-          <img src="../assets/wayLogo.png" @click="$router.push('showWay')"/>
-      </div>
-      <div id="buttonnav">
-          <img src="../assets/patientinfosLogo.png" @click="$router.push('patientInfos')"/>
+          <img src="../assets/patientinfosLogo.png" @click="$router.push('patientInfos')" v-if="aktUser.role == '2.0'"/>
       </div>
       <div id="buttonnav">
           <img src="../assets/writingReport.png" @click="$router.push('missionOverview')"/>
       </div>
       <div id="buttonnav">
-          <img src="../assets/writingReport.png" @click="$router.push('createMission')" />
+          <img src="../assets/writingReport.png" @click="$router.push('createMission')" v-if="aktUser.role == '1.0'"/>
       </div>
       <div id="buttonnav">
           <img src="../assets/writingReport.png" @click="$router.push('admin')"/>
